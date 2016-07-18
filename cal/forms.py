25ac2eb95d7ba5bfde48291332ddf477c837cd38
Jack Anteby <Janteby1@gmail.com>
2016-07-18 18:53:50 -0400

    started dad website, need to finish form

--- cal/forms.py
+++ cal/forms.py
@@ -2,110 +2,17 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import Textarea, CheckboxInput, PasswordInput
-from .models import UserProfile, Events, Organization, Tags, TaggedTag
+from .models import Scores
 
+class AddScoresForm(forms.ModelForm):
+    player = forms.CharField() # get the input directly from the user
+    court = forms.CharField() # get the input directly from the user
+    won = forms.CharField() # get the input directly from the user
+    loss = forms.CharField() # get the input directly from the user
 
-# form used to register a user
-class UserForm(UserCreationForm):
     class Meta:
-        model = User
-        fields = ("first_name", "last_name", "username", "email", "password1", "password2")
-
-
-class OrgForm(forms.ModelForm):
-    class Meta:
-        model = Organization
+        model = Scores
         fields = [
-            "name",
-            "email",
-            "phone", 
-            "website",
-            "industry"
-        ]
-        widgets = {
-            # this sets the input text area
-            "password": PasswordInput(),
-        }
-
-
-class UserLoginForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = [
-            "username",
-            "password",
-        ]
-        widgets = {
-            # this sets the input text area
-            "password": PasswordInput(),
-        }
-
-
-class AddEventForm(forms.ModelForm):
-    date = forms.CharField() # get the input directly from the user
-    place = forms.CharField() # get the input directly from the user
-    address = forms.CharField() # get the input directly from the user
-
-    price = forms.CharField() # get the input directly from the user
-    description = forms.CharField() # get the input directly from the user
-    phone = forms.CharField() # get the input directly from the user
-    website = forms.CharField() # get the input directly from the user
-
-    class Meta:
-        model = Events
-        fields = [
-            'date','place','address',
-            'price','description','phone','website'
+            'player','court','won', 'loss',
         ]
 
-
-# class AddTagsForm(forms.ModelForm):
-#     tags = forms.CharField() # get the input directly from the user
-
-#     class Meta:
-#         model = TaggedTag
-#         fields = ['tag', 'content_type', 'object_id', 'tagged_item']
-
-
-# class OrgLoginForm(forms.ModelForm):
-#     class Meta:
-#         model = Organization
-#         fields = [
-#             "username",
-#             "password",
-#         ]
-#         widgets = {
-#             # this sets the input text area
-#             "password": PasswordInput(),
-#         }
-
-
-# class AddEventForm(forms.Form):
-#     date = forms.CharField() # get the input directly from the user
-#     place = forms.CharField() # get the input directly from the user
-#     address = forms.CharField() # get the input directly from the user
-
-#     price = forms.CharField() # get the input directly from the user
-#     description = forms.CharField() # get the input directly from the user
-#     phone = forms.CharField() # get the input directly from the user
-#     website = forms.CharField() # get the input directly from the user
-
-#     def save(self, commit=True):
-#         event = Events(**self.cleaned_data)
-#         if commit == True:
-#             super().save()
-#         print (event)
-#         return event
-
-
-'''    
-    organization = models.ForeignKey(Organization) # FK to the Organization table
-    Tags
-'''
-
-
-
-
-
-
-        
--- cal/multiples.py
+++
@@ -1,8 +0,0 @@
-def multiples(n):
-	total = 0
-	for i in range(n):
-		if i%3 == 0 or i%5 == 0:
-			total = toal + i 
-	return total 
-
-multiples(10)
